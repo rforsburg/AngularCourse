e2a0ba0eae18fc8d5134d3d116bb675ceba3c75c
Richard Forsburg <rforsburg@stny.rr.com>
2021-07-25 14:02:37 -0400

    Week 3 Observable to Promise

--- src/app/services/promotion.service.ts
+++ src/app/services/promotion.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import {Promotion } from '../shared/promotion'
 import {PROMOTIONS} from '../shared/promotions'
-
+import { of } from 'rxjs';
+import { delay } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -11,21 +12,29 @@ export class PromotionService {
   constructor() { }
 
   getPromotions(): Promise <Promotion[]>{     
-    return new Promise(resolve => {
-      setTimeout(() => resolve(PROMOTIONS),2000)
-    })
+    return of (PROMOTIONS).pipe(delay(2000)).toPromise()
+    }
+
+    // return new Promise(resolve => {
+    //   setTimeout(() => resolve(PROMOTIONS),2000)
+    // })
+    // }
+
+  getPromotion(id: string): Promise <Promotion> { 
+    return of(PROMOTIONS.filter((promo) => (promo.id === id))[0]).pipe(delay(2000)).toPromise()
   }
 
-  getPromotion(id: string): Promise <Promotion>{  
-    return new Promise( resolve => {
-    setTimeout(() => resolve(PROMOTIONS.filter((promo)  => id === promo.id)[0]),2000)
-  })
-}
+  //   return new Promise( resolve => {
+  //   setTimeout(() => resolve(PROMOTIONS.filter((promo)  => id === promo.id)[0]),2000)
+  // })
+  // }
 
   getFeaturedPromotion(): Promise <Promotion> {
-    return new Promise(resolve => {
-      setTimeout(() => resolve(PROMOTIONS.filter((promotion) => promotion.featured )[0]),2000)
-    })
+    return of(PROMOTIONS.filter((promotion) => promotion.featured )[0]).pipe(delay(2000)).toPromise()
   }
 
+//   return new Promise(resolve => {
+//     setTimeout(() => resolve(PROMOTIONS.filter((promotion) => promotion.featured )[0]),2000)
+//   })
+// }
 }
